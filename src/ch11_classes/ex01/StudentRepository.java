422278968f70027c9dc0131903d1dfee31f3b863
beckgo34 <beckgo34@naver.com>
2023-12-14 16:01:23 +0900

    클래스 분리 예제 - 메인 메뉴 구성 시작

--- src/ch11_classes/ex01/StudentRepository.java
+++ src/ch11_classes/ex01/StudentRepository.java
@@ -1,4 +1,10 @@
 package ch11_classes.ex01;
 
 public class StudentRepository {
+    public void method1(){
+        System.out.println("StudentRepository.method1");
+    }
+    public void method2(String str1){
+        System.out.println("str1 = " + str1);
+    }
 }
