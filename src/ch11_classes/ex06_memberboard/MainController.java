40563e79249c8e376a9054e8359221593bdd279c
beckgo34 <beckgo34@naver.com>
2023-12-22 09:22:56 +0900

    클래스 분리 예제 - 회원제 게시판 메뉴선택 완성

--- src/ch11_classes/ex06_memberboard/MainController.java
+++ src/ch11_classes/ex06_memberboard/MainController.java
@@ -12,6 +12,7 @@ public class MainController {
         MemberService memberService = new MemberService();
         BoardService boardService = new BoardService();
         boolean run = true;
+        boolean runBoard = true;
         int selectNo = 0;
         int selectNoBoard = 0;
         while (run) {
@@ -35,30 +36,34 @@ public class MainController {
                 memberService.logout();
             } else if (selectNo == 7) {
                 if (CommonVariables.loginEmail != null) {
-                    System.out.println("====== 게시판 ======");
-                    System.out.println("-----------------------------------------------------------------------------------------");
-                    System.out.println("1.글작성 | 2.글목록 | 3.글조회 | 4.글수정 | 5.글삭제 | 6.검색 | 99.sample | 0.메인메뉴");
-                    System.out.println("-----------------------------------------------------------------------------------------");
-                    System.out.print("선택> ");
-                    selectNoBoard = scanner.nextInt();
-                    if (selectNoBoard == 1) {
-                        boardService.boardWrite();
-                    } else if (selectNoBoard == 2) {
-                        boardService.boardList();
-                    } else if (selectNoBoard == 3) {
-                        boardService.boardFind();
-                    } else if (selectNoBoard == 4) {
-                        boardService.boardUpdate();
-                    } else if (selectNoBoard == 5) {
-                        boardService.boardDelete();
-                    } else if (selectNoBoard == 6) {
-                        boardService.boardSearch();
-                    } else if (selectNoBoard == 0) {
-                        run = false;
+                    while (runBoard) {
+                        System.out.println("====== 게시판 ======");
+                        System.out.println("-----------------------------------------------------------------------------------------");
+                        System.out.println("1.글작성 | 2.글목록 | 3.글조회 | 4.글수정 | 5.글삭제 | 6.검색 | 99.sample | 0.메인메뉴");
+                        System.out.println("-----------------------------------------------------------------------------------------");
+                        System.out.print("선택> ");
+                        selectNoBoard = scanner.nextInt();
+                        if (selectNoBoard == 1) {
+                            boardService.boardWrite();
+                        } else if (selectNoBoard == 2) {
+                            boardService.boardList();
+                        } else if (selectNoBoard == 3) {
+                            boardService.boardFind();
+                        } else if (selectNoBoard == 4) {
+                            boardService.boardUpdate();
+                        } else if (selectNoBoard == 5) {
+                            boardService.boardDelete();
+                        } else if (selectNoBoard == 6) {
+                            boardService.boardSearch();
+                        } else if (selectNoBoard == 0) {
+                           runBoard = false;
+                        }
                     }
                 } else {
                     System.out.println("로그인이 필요한 서비스입니다");
                 }
+            } else if (selectNo == 0) {
+                run =false;
             }
         }
     }
