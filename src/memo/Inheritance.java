0bf1e189fbec5cc8ea30f31b4b61b3c29954a484
beckgo34 <beckgo34@naver.com>
2023-12-26 15:16:31 +0900

    상속

---
+++ src/memo/Inheritance.java
@@ -0,0 +1,21 @@
+package memo;
+
+public class Inheritance {
+    /**
+     * 상속(Inheritance)
+     *   *부모클래스의 자원(필드, 생성자, 메서드)을 자식클래스에게 물려주는 것
+     *      -물려준다고 해서 부모의 자원이 사라지는 것은 아님
+     *      -부모클래스에서 private으로 지정한 자원은 상속 불가
+     *   *동시에 두 개 이상의 클래스 상속 불가
+     *   1.문법
+     *      a.extends 키워드 사용
+     *          // 부모클래스
+     *          public class ParentClass{
+     *
+     *          }
+     *          // 자식클래스
+     *          public class ChildClass extends parentClass{
+     *
+     *          }
+     */
+}
