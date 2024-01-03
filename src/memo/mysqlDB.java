8045e2464b040e78c3446e53385959c1f71576e2
beckgo34 <beckgo34@naver.com>
2024-01-03 16:08:37 +0900

    db 1/3 오후

--- src/memo/mysqlDB.java
+++ src/memo/mysqlDB.java
@@ -294,5 +294,141 @@ insert into table1(col1, col2) values(5, 'asdfgh');
     select e.empno, e.ename, e.deptno, d.dname, d.loc from emp e, dept d where e.deptno = d.deptno and e.sal >= 2500 order by e.ename asc;
 -- 1/3 오전끝
 
+-- 1/4 오후 시작
+
+-- 서브쿼리(sub query)
+-- 하나의 쿼리문 안에서 다른 쿼리문을 수행하는 것
+-- 최저 급여를 받는 사원의 이름
+select * from emp;
+-- 1. 최저급여거 얼마인지? => 800
+select min(sal) from emp;
+-- 2. 800 급여를 받는 사원의 이름 => smith
+select ename from emp where sal = 800;
+-- 위의 두 문장을 서브쿼리로
+select ename from emp where sal=(select min(sal) from emp);
+-- 최고 급여를 받는 사원의 정보 조회
+select * from emp where sal=(select max(sal) from emp);
+-- allen 보다 높은 급여를 받는 사원 조회
+select sal from emp where ename = 'allen';
+select * from emp where sal > 1600;
+select * from emp where sal > (select sal from emp where ename = 'allen');
+
+
+	연습문제
+    1. clark 보다 늦게 입사한 사원 조회
+    2. 부서번호가 20인 사원 중에서 전체 사원 평균 급여보다 높은 급여를 받는 사원 조회
+    3. 2번 조회 결과에서 부서이름, 부서위치도 함께 조회
+
+-- 1.clark 보다 늦게 입사한 사원 조회
+    select * from emp where hiredate > (select hiredate from emp where ename = 'clark');
+-- 2. 부서번호가 20인 사원 중에서 전체 사원 평균 급여보다 높은 급여를 받는 사원 조회
+    select * from emp where deptno = 20 and sal > (select avg(sal) from emp);
+    select * from emp where sal > (select avg(sal) from emp) and deptno = 20;
+--  3. 2번 조회 결과에서 부서이름, 부서위치도 함께 조회
+    select * from emp e, dept d where e.deptno = d.deptno and sal > (select avg(sal) from emp) and e.deptno = 20;
+-- 4. martin과 같은 부서 사원 중에서 전체 사원 평균 급여보다 높은 급여를 받는 사원 조회
+    select deptno from emp where ename = 'martin';
+    select * from emp where sal > (select avg(sal) from emp) and deptno = (select deptno from emp where ename = 'martin');
+
+-- 제약조건
+-- id, member_email, member_password 클럼이 있는 테이블
+    drop table if exists member1;
+    create table member1(
+            id bigint,
+            member_email varchar(20),
+    member_password varchar(20)
+);
+    insert into member1(id, member_email, member_password) values(1, 'aa@aa.com', '1234');
+-- 모든 컬럼에 값을 다 넣을 때는 컬럼이름 생략 가능
+    insert into member1 values(2, 'bb@bb.com', '1234');
+-- 특정 컬럼만 값을 넣을 때는
+    insert into member1 values(3, 'cc@cc.com');
+    insert into member1(id, member_email) values(3, 'cc@cc.com');
+    select * from member1;
+    insert into member1 values(4, 'dd@dd.com', null);
+
+    drop table if exists member2;
+    create table member2(
+            id bigint not null,
+            member_email varchar(20),
+    member_password varchar(20)
+);
+    select * from member2;
+    insert into member2(id, member_email, member_password) values(1, 'aa@aa.com', '1234');
+-- id를 제외하고 나머지 값만 입력
+    insert into member2(member_email, member_password) values('bb@bb.com', '1234');
+    insert into member2(id, member_email, member_password) values(null, 'cc@cc.com', '1234');
+    insert into member2(id, member_email, member_password) values(4, null, '1234');
+
+    drop table if exists member3;
+    create table member3(
+            id bigint not null unique,
+            member_email varchar(20) not null,
+    member_password varchar(20) not null
+            );
+    select * from member3;
+    insert into member3(id, member_email, member_password) values(1, 'aa@aa.com', '1234');
+    insert into member3(id, member_email, member_password) values(1, 'bb@bb.com', '1234');
+    insert into member3(id, member_email, member_password) values(2, 'bb@bb.com', not null);
+
+    drop table if exists member4;
+    create table member4(
+            id bigint not null unique,
+            member_email varchar(20) not null unique,
+    member_password varchar(20) not null,
+    created_at datetime
+);
+    select * from member4;
+    insert into member4(id, member_email, member_password, created_at) values(1, 'aa@aa.com', '1234', now());
+    insert into member4(id, member_email, member_password) values(2, 'bb@bb.com', '1234');
+
+    drop table if exists member5;
+    create table member5(
+            id bigint not null unique,
+            member_email varchar(20) not null unique,
+    member_password varchar(20) not null,
+    created_at datetime default now()
+);
+    select * from member5;
+-- default로 지정한 컬럼은 따로 값을 주지 않아도 값이 default로 지정한 값으로 저장됨
+    insert into member5(id, member_email, member_password, created_at) values(1, 'aa@aa.com', '1234', now());
+    insert into member5(id, member_email, member_password) values(2, 'bb@bb.com', '1234');
+
+-- primary key
+    drop table if exists member6;
+    create table member6(
+            id bigint primary key,
+            member_email varchar(20) not null unique,
+    member_password varchar(20) not null,
+    created_at datetime default now()
+);
+    select * from member6;
+    insert into member6(id, member_email, member_password) values(1, 'aa@aa.com', '1234');
+    insert into member6(id, member_email, member_password) values(2, 'bb@bb.com', '1234');
+
+
+    drop table if exists member7;
+    create table member7(
+            id bigint,
+            member_email varchar(20) not null unique,
+    member_password varchar(20) not null,
+    created_at datetime default now(),
+    constraint pk_member7 primary key(id)
+);
+-- 자동 번호 적용하기 (auto_increment는 pk 컬럼에만 지정가능)
+    drop table if exists member8;
+    create table member8(
+            id bigint auto_increment,
+            member_email varchar(20) not null unique,
+    member_password varchar(20) not null,
+    created_at datetime default now(),
+    -- id2 bigint auto_increment, -- 일반 컬럼에는 지정 불가
+    constraint pk_member8 primary key(id)
+);
+    select * from member8;
+-- auto_increment를 지정하면 값을 따로 주지 않아도 됨
+    insert into member8(member_email, member_password) values('aa@aa.com', '1234');
+    insert into member8(member_email, member_password) values('bb@bb.com', '1234');
+
 */
 }
