d06edd60f3b44a473d9d68f0c70ad19a0f69c657
beckgo34 <beckgo34@naver.com>
2024-01-03 11:19:01 +0900

    db 1/3 오전

--- src/memo/mysqlDB.java
+++ src/memo/mysqlDB.java
@@ -214,5 +214,85 @@ insert into table1(col1, col2) values(5, 'asdfgh');
     select count(*) from emp;
 -- 부서번호가 20인 사원수
     select count(*) from emp where deptno = 20;
-     */
+
+    -- 1/3 오전
+-- 그룹화(grouping, 특정 컬럼을 동일한 데이터끼리 묶는것)
+-- 그룹화한 결과를 조회할 때는 그룹화한 컬럼으로 조회만 가능
+-- job을 기준으로
+    select * from emp group by job; -- x
+    select job, ename from emp group by job; -- x
+    select job from emp group by job;
+    select job, ename from emp group by job, ename;
+-- 직급별 평균급여
+    select job, avg(sal) from emp group by job;
+    select job, round(avg(sal),2) from emp group by job;
+-- 직급이 clerk인 사원들의 평균급여
+    select job, avg(sal) from emp where job = 'clerk';
+-- 부서별(deptno) 평균급여
+    select deptno, avg(sal) from emp group by deptno;
+-- 직급별 사원수
+    select job,count(*) from emp group by job;
+-- 직급별 평균급여, 사원수
+    select job,avg(sal),count(*) from emp group by job;
+-- 위의 결과에서 직급기준 오름차순 정렬
+    select job,avg(sal),count(*) from emp group by job order by job;
+-- 위의 결과에서 평균 급여 소수 첫째자리 까지
+    select job, round(avg(sal) ,1),count(*) from emp group by job order by job;
+-- 위의 결과에서 job은 '직급' 급여 평균값은 '평균급여', 사원수는 '사원수'로 표현하여 조회
+    select job as '직급', round(avg(sal) ,1) as '평균급여',count(*) as '사원수' from emp group by job order by job;
+
+-- 두 개 이상 컬럼 그룹화
+-- deptno를 기준은로 그룹화하고 그 결과내에서 job 기준으로 그룹화
+    select deptno, job, count(*) from emp group by deptno, job;
+    select deptno, job, count(*), avg(sal) from emp group by deptno, job;
+    select deptno, job, count(*), round(avg(sal), 1) from emp group by deptno, job order by deptno;
+
+-- 그룹화한 결과에 조건을 적용
+-- having
+-- 위의 결과에서 평균 급여가 2000 이상인 결과만 조회
+    select deptno, job, count(*), avg(sal) from emp group by deptno, job having avg(sal) >= 2000;
+    select deptno, job, count(*), avg(sal) from emp group by deptno, job having avg(sal) >= 2000 order by deptno asc;
+-- 급여가 3000 이하인 사원만 가지고 부서별, 직급별 그룹화를 하고 평균급여가 2000 이상인 결과 조회
+    select deptno, job, count(*), avg(sal) from emp where sal <= 3000 group by deptno, job having avg(sal) >= 2000 order by deptno;
+
+-- date 타입으로 문자 표현: date_format()
+    select date_format(hiredate, '%Y') from emp;
+
+
+	연습문제
+	1. 부서별 평균급여, 최고급여, 최저급여, 사원수 조회(평균급여는 소수점 둘째자리에서 반올림)
+    2. 직급별 사원수 조회(단 3명 이상인 결과만 출력)
+    3. 연도별 입사한 사원수 조회(조회결과 : 연도(yyyy), 사원수)
+    3.1. 위의 결과에서 각 연도별로 부서별 입사한 사원수 조회(조회결과 : 연도(yyyy), 부서번호, 사원수)
+
+-- 1. 부서별 평균급여, 최고급여, 최저급여, 사원수 조회(평균급여는 소수점 둘째자리에서 반올림)
+    select deptno as '부서별', round(avg(sal), 1) as '평균급여', max(sal) as '최고급여', min(sal) as '최저급여', count(*) as '사원수' from emp group by deptno order by deptno asc;
+--  2. 직급별 사원수 조회(단 3명 이상인 결과만 출력)
+    select job, count(empno) from emp group by job having count(*) >= 3;
+-- 연도별 입사한 사원수 조회(조회결과 : 연도(yyyy), 사원수)
+    select date_format(hiredate, '%Y'), count(*) from emp group by date_format(hiredate, '%Y');
+-- 위의 결과에서 각 연도별로 부서별 입사한 사원수 조회(조회결과 : 연도(yyyy), 부서번호, 사원수)
+    select date_format(hiredate, '%Y'), deptno, count(*) from emp group by date_format(hiredate, '%Y'), deptno order by date_format(hiredate, '%Y');
+
+-- 조인(join)
+-- 두 개 이상의 테이블을 하나의 테이블처럼 사용할 때
+    select * from emp;
+    select * from dept;
+    select * from emp, dept;
+-- 내부조인(inner join)
+-- 조인하려는 두 테이블에 공통 컬럼이 존재해야 함.
+            select * from emp, dept where emp.deptno = dept.deptno;
+-- 테이블 이름을 줄여서 사용
+    select * from emp e, dept d where e.deptno = d.deptno;
+-- deptno 컬럼은 두 테이블에 모두 있기 때문에 ambiguous 에러
+    select empno, ename, job, deptno, dname, loc from emp e, dept d where e.deptno = d.deptno;
+    select e.empno, e.ename, e.job, e.deptno, dname, loc from emp e, dept d where e.deptno = d.deptno;
+-- emp 테이블 전체 조회
+    select e.* from emp e , dept d where e.deptno = d.deptno;
+-- emp, dept를 조인하여 empno, ename, deptno, dname, loc 조회
+-- (단, 급여가 2500 이상인 사원만 조회하고, 조회결과는 사원이름 기준으로 오름차순 정렬)
+    select e.empno, e.ename, e.deptno, d.dname, d.loc from emp e, dept d where e.deptno = d.deptno and e.sal >= 2500 order by e.ename asc;
+-- 1/3 오전끝
+
+*/
 }
