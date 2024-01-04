e4f1389f173692c02ce9e322bb4245c509ddf0bc
beckgo34 <beckgo34@naver.com>
2024-01-04 11:35:23 +0900

    db 1/4 오전 테이블 예제 완료

--- src/memo/mysqlDB.java
+++ src/memo/mysqlDB.java
@@ -538,5 +538,64 @@ insert into child3(id, c1, c2, p_id) values(4, 'ddd', 'ddd', 5);
 update child3 set c1= '수정내용' where id=2;
 update child3 set c1= '수정내용', c2 = 'ㅎㅎㅎ' where id=2;
 
+drop table if exists book;
+create table book(
+	id bigint auto_increment,
+    b_bookname varchar(20),
+    b_publisher varchar(20),
+    b_price int,
+    constraint pk_book primary key(id)
+);
+insert into book(b_bookname, b_publisher, b_price) values('축구 역사', '좋은출판사', 7000);
+insert into book(b_bookname, b_publisher, b_price) values('축구 리포트', '나무출판사', 13000);
+insert into book(b_bookname, b_publisher, b_price) values('축구를 알려주마', '대한출판사', 22000);
+insert into book(b_bookname, b_publisher, b_price) values('배구의 바이블', '대한출판사', 35000);
+insert into book(b_bookname, b_publisher, b_price) values('피겨 교과서', '좋은출판사', 8000);
+insert into book(b_bookname, b_publisher, b_price) values('피칭의 단계별기술', '좋은출판사', 6000);
+insert into book(b_bookname, b_publisher, b_price) values('야구의 추억 이야기', '나이스미디어', 20000);
+insert into book(b_bookname, b_publisher, b_price) values('야구 읽어주는 남자', '나이스미디어', 13000);
+insert into book(b_bookname, b_publisher, b_price) values('올림픽 스토리', '이야기당', 7500);
+insert into book(b_bookname, b_publisher, b_price) values('olympic history', 'strawberry', 13000);
+select * from book;
+
+drop table if exists customer;
+create table customer(
+	id bigint auto_increment,
+    c_name varchar(10),
+    c_address varchar(20),
+    c_phone varchar(15),
+    constraint pk_customer primary key (id)
+);
+
+insert into customer(c_name, c_address, c_phone) values('손흥민', '영국 런던', '000-5000-0001');
+insert into customer(c_name, c_address, c_phone) values('김연아', '대한민국 서울', '000-6000-0001');
+insert into customer(c_name, c_address, c_phone) values('김연경', '대한민국 서울', '000-7000-0001');
+insert into customer(c_name, c_address, c_phone) values('류현진', '캐나다 토론토', '000-8000-0001');
+insert into customer(c_name, c_address, c_phone) values('이강인', '프랑스 파리', '000-9000-0001');
+select * from customer;
+
+drop table if exists orders;
+create table orders(
+	id bigint auto_increment,
+    customer_id bigint,
+    book_id bigint,
+    o_saleprice int,
+    o_oderdate date,
+    constraint fk_orders1 foreign key (book_id) references book(id),
+    constraint fk_orders2 foreign key (customer_id) references customer(id),
+    constraint pk_orders primary key (id)
+);
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(1, 1, 6000, str_to_date('01-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(1, 3, 21000, str_to_date('03-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(2, 5, 8000, str_to_date('03-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(3, 6, 6000, str_to_date('04-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(4, 7, 20000, str_to_date('05-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(1, 2, 12000, str_to_date('07-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(4, 8, 13000, str_to_date('07-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(3, 10, 12000, str_to_date('08-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(2, 10, 7000, str_to_date('09-07-2023','%d-%m-%Y'));
+insert into orders(customer_id, book_id, o_saleprice, o_oderdate) values(3, 8, 13000, str_to_date('10-07-2023','%d-%m-%Y'));
+select * from orders;
+
 */
 }
