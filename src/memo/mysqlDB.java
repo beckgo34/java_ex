d248ad9145de6817a1f54c1e6313bb25e2a609d5
beckgo34 <beckgo34@naver.com>
2024-01-05 11:21:26 +0900

    db 1/5 오전 수업 끝

--- src/memo/mysqlDB.java
+++ src/memo/mysqlDB.java
@@ -794,5 +794,78 @@ select
          where c.id = o.customer_id
          and c.c_name = '손흥민';
 
+-- 1/5 오전 시작
+-- 30. 가장 비싼 도서의 이름을 조회
+select
+	b_bookname
+	from book
+    where b_price = (select max(b_price) from book);
+
+-- 31. 책을 구매한 이력이 있는 고객의 이름을 조회
+select
+	*
+    from orders;
+
+select
+	c_name
+    from customer
+		where id in(1,2,3,4);
+
+select customer_id from orders;
+
+select
+	c_name
+    from customer
+		where id
+			in(select customer_id from orders);
+-- 32. 도서의 가격(PRICE)과 판매가격(SALEPRICE)의 차이가 가장 많이 나는 주문 조회
+select * from book;
+select * from orders;
+-- book, orders 조인해서 정가와 판매가 차이
+select b.b_price-o.o_saleprice from book b, orders o where b.id = o.bookid;
+select max(b.b_price-o.o_saleprice) from book b, orders o where b.id = o.bookid;
+select * from book b, orders o where b.id = o.bookid and b.b_price-o.o_saleprice=6000;
+select * from book b, orders o where b.id = o.bookid and b.b_price-o.o_saleprice= (select max(b.b_price-o.o_saleprice) from book b, orders o where b.id = o.bookid);
+-- 33. 고객별 평균 구매 금액이 도서의 판매 평균 금액 보다 높은 고객의 이름 조회
+-- 도서 판매 평균 금액
+select avg(o_saleprice) from orders;
+-- 고객별 평균 구맥 금액{group by)
+select c.c_name, avg(o.o_saleprice) from orders o , customer c where c.id=o.customer_id group by c.c_name;
+select c.c_name, avg(o.o_saleprice) from orders o , customer c where c.id=o.customer_id group by c.c_name having avg(o.o_saleprice) > (select c.c_name, avg(o.o_saleprice) from orders);
+
+-- 34. 고객번호가 5인 고객의 주소를 대한민국 인천으로 변경
+
+update
+	customer
+    set c_address = '대한민국 인천'
+    where id = 5;
+
+-- 35. 김씨 성을 가진 고객이 주문한 총 판매액 조회
+select id from customer where c_name like '김%';
+select sum(o_saleprice) from orders where customer_id=2 or customer_id=3;
+select sum(o_saleprice) from orders where customer_id in(2,3);
+select sum(o_saleprice) from orders where customer_id in(select id from customer where c_name like '김%');
+
+-- alter: 테이블의 구조를 변경할 때 (컬럼이름 변경, 타입 변경, 제약조건 추가 등)
+create table student(
+	id bigint,
+    s_name varchar(20),
+    s_mobile int
+);
+-- 테이블의 구조 확인
+desc student;
+desc book;
+-- 기존 컬럼에 제약조건 추가
+alter table student add constraint primary key(id);
+-- 기존 컬럼에 타입 변경
+alter table student modify s_mobile varchar(30);
+-- 컬럼 추가
+alter table student add s_major varchar(30);
+-- 컬럼 이름 변경
+alter table student change s_mobile s_phone varchar(30);
+-- 컬럼 삭제
+alter table student drop s_major;
+-- 1/5 오전 수업 끝
+
 */
 }
