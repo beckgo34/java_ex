33fe2e58025578061c9adfac860e4206aab16762
beckgo34 <beckgo34@naver.com>
2024-01-09 13:29:34 +0900

    프론트엔드

---
+++ src/memo/frontend.java
@@ -0,0 +1,43 @@
+package memo;
+
+public class frontend {
+    /**
+     *  03_ Fronend
+     *   * 사용자가 웹서비스를 활용할 때 만나는 화면
+     *   * 화면,UI(User Interface), GUI(Graphical User Interface)
+     *   * react, vue
+     *   * 웹개발자, 웹디자이너, 웹퍼블리셔
+     *
+     *   1. 구석요소
+     *      a. HTML (HyperText Markup language)
+     *          1. 화면의 구조를 정의
+     *          2. 화면에 나오는 텍스트, 이미지 등을 보여줌
+     *          3. 여러 태그, 속성, 속성ㄱ밧 등으로 세부적인 특성 등을 정의
+     *      b. CSS (Cascading Style Sheets)
+     *          1. 화면의 레이아웃, 디자인 등을 꾸밀 수 있음
+     *      C. JavaScript
+     *          1. 화면을 동적으로 변화시킬 수 있는 기능
+     *          2. 여러 api 연동 등을 통해 다양한 기능을 추가하는 코드 작성
+     *          3. 프로그래밍 언어의 한 종류
+     *   2. 파일
+     *      a. 확장자: *.html
+     *      b. html 파일안에 모든 내용을 작성
+     *      c. .css, .js 파일을 따로 분리하여 사용가능
+     *   3. html 태그
+     *      a. <> 로 표현함
+     *      b. html, head, body, div, p, table 등의 고유 태그들이 있으며, 필요시 직접 태그를 만들수도 있음.
+     *      c. 시작태그, 졸료태그로 태그의 범위를 지정함.(자바에서의 중괄호 역할과 비슷)
+     *          1.
+     *              <body> body 시작태그
+     *                 div, p 태그는 모두 body 태그 소속
+     *                 <div> div 시작태그
+     *                     div 의 영역
+     *
+     *                 </div>  div 종료태그
+     *                 <p> p 시작태그
+     *                     p 의 영역
+     *                 </p> p 종료태그
+     *              </body> body 태그 종료
+     *
+     */
+}
